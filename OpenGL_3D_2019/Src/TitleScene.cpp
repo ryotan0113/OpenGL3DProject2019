8a92c211d794190c8739221625e40227e71da8dd
稜太 瀬川 <ryotan.113@outlook.jp>
2019-05-14 19:00:42 +0900

    タイトル画面のProcessInput関数を定義

---
+++ OpenGL_3D_2019/Src/TitleScene.cpp
@@ -0,0 +1,13 @@
+/**
+* @file TitleScene.cpp
+*/
+#include "TitleScene.h"
+#include "MainGameScene.h"
+
+/**
+* ƒvƒŒƒCƒ„[‚Ì“ü—Í‚ğˆ—‚·‚é
+*/
+void TitleScene::ProcessInput()
+{
+	SceneStack::Instance().Replace(std::make_shared<MainGameScene>());
+}
