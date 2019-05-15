820afff752369cbbc45ccb77a15b974c4b9126ec
稜太 瀬川 <ryotan.113@outlook.jp>
2019-05-15 17:11:23 +0900

    GLFWEW.hの変更

--- OpenGL_3D_2019/Src/GLFWEW.h
+++ OpenGL_3D_2019/Src/GLFWEW.h
@@ -4,8 +4,8 @@
 #ifndef GLFWEW_H_INCLUDED
 #define GLFWEW_H_INCLUDED
 #include <GL/glew.h>
+#include "GamePad.h"
 #include <GLFW/glfw3.h>
-#include <glm/vec2.hpp>
 
 namespace GLFWEW {
 
@@ -27,20 +27,23 @@ namespace GLFWEW {
 
         bool IsKeyDown(int key) const;
         bool IsKeyPressed(int key) const;
-        glm::vec2 GetMousePosition() const;
         int GetMouseButton(int button) const;
+
+		const GamePad& GetGamePad() const;
 
 	private:
 		Window();
 		~Window();
 		Window(const Window&) = delete;
 		Window& operator=(const Window&) = delete;
+		void UpdateGamePad();
 
 		bool isGLFWInitialized = false;
 		bool isInitialized = false;
 		GLFWwindow* window = nullptr;
         double previousTime = 0;
         double deltaTime = 0;
+		GamePad gamepad;
 
         enum class KeyState : char {
           release,
