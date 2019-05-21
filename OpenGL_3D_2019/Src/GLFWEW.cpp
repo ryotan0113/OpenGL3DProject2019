6b895e4cf90913cf7d14d0fa66c323ee007aa175
稜太 瀬川 <ryotan.113@outlook.jp>
2019-05-21 18:44:27 +0900

    ゲームパッドの関数を定義

--- OpenGL_3D_2019/Src/GLFWEW.cpp
+++ OpenGL_3D_2019/Src/GLFWEW.cpp
@@ -126,7 +126,127 @@ enum GAMEPAD_AXES
 	GAMEPAD_AXES_TRIGGER, ///< アナログトリガー,
 	GAMEPAD_AXES_RIGHT_Y, ///<右スティックのY軸,
 	GAMEPAD_AXES_RIGHT_X, ///<右スティックのX軸,
+	
 };
+
+/**
+* ゲームパッドのデジタル入力装置ID.
+*
+* @note Xbox360ゲームパッド準拠
+*/
+enum GAMEPAD_BUTTON {
+	GAMEPAD_BUTTON_A, ///< Aボタン
+	GAMEPAD_BUTTON_B, ///< Bボタン
+	GAMEPAD_BUTTON_X, ///< Xボタン
+	GAMEPAD_BUTTON_Y, ///< Yボタン
+	GAMEPAD_BUTTON_L, ///< Lボタン
+	GAMEPAD_BUTTON_R, ///< Rボタン
+	GAMEPAD_BUTTON_BACK, ///< Backボタン
+	GAMEPAD_BUTTON_START, ///< Startボタン
+	GAMEPAD_BUTTON_L_THUMB, ///< 左スティックボタン
+	GAMEPAD_BUTTON_R_THUMB, ///< 右スティックボタン
+	GAMEPAD_BUTTON_UP, ///< 上キー
+	GAMEPAD_BUTTON_RIGHT, ///< 右キー
+	GAMEPAD_BUTTON_DOWN, ///< 下キー
+	GAMEPAD_BUTTON_LEFT, ///< 左キー
+};
+
+/**
+* ゲームパッドの状態を更新する
+*/
+void Window::UpdateGamePad()
+{
+	const uint32_t prevButtons = gamepad.buttons; // buttonDownを生成するために、更新前の入力
+
+	//アナログ入力とボタン入力を取得
+	int axesCount, buttonCount;
+	const float* axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axesCount);
+	const uint8_t* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
+
+	//両方の配列がnullptrでなく、最低限必要なデータ数を満たしていれば、有効なゲームパッドが接続される
+	if (axes && buttons && axesCount >= 2 && buttonCount >= 8) {
+		//有効なゲームパッドが接続されている場合
+
+		//方向キーの入力状態を消去して、左スティックの入力で置き換える
+		gamepad.buttons &= ~(GamePad::DPAD_UP | GamePad::DPAD_DOWN | GamePad::DPAD_LEFT | GamePad::DPAD_RIGHT);
+		static const float digitalThreshold = 0.3f; //デジタル入力とみなすしきい値
+		if (axes[GAMEPAD_AXES_LEFT_Y] >= digitalThreshold) {
+			gamepad.buttons |= GamePad::DPAD_UP;
+		}
+		else if (axes[GAMEPAD_AXES_LEFT_Y] <= -digitalThreshold) {
+			gamepad.buttons |= GamePad::DPAD_DOWN;
+		}
+		if (axes[GAMEPAD_AXES_LEFT_X] >= digitalThreshold) {
+			gamepad.buttons |= GamePad::DPAD_LEFT;
+		}
+		else if (axes[GAMEPAD_AXES_LEFT_X] <= -digitalThreshold) {
+			gamepad.buttons |= GamePad::DPAD_RIGHT;
+		}
+
+		//配列インデックスとGamePadキーの対応表
+		static const struct {
+			int dataIndex;
+			uint32_t gamepadBit;
+		} keyMap[] = {
+			{GAMEPAD_BUTTON_A, GamePad::A},
+			{GAMEPAD_BUTTON_B, GamePad::B},
+			{GAMEPAD_BUTTON_X, GamePad::X},
+			{GAMEPAD_BUTTON_Y, GamePad::Y},
+			{GAMEPAD_BUTTON_L, GamePad::L},
+			{GAMEPAD_BUTTON_R, GamePad::R},
+			{GAMEPAD_BUTTON_START, GamePad::START},
+			{GAMEPAD_BUTTON_UP, GamePad::DPAD_UP},
+			{GAMEPAD_BUTTON_DOWN, GamePad::DPAD_DOWN},
+			{GAMEPAD_BUTTON_LEFT, GamePad::DPAD_LEFT},
+			{GAMEPAD_BUTTON_RIGHT, GamePad::DPAD_RIGHT},
+
+		};
+
+		for (const auto& e : keyMap) {
+			if (buttons[e.dataIndex] == GLFW_PRESS) {
+				gamepad.buttons |= e.gamepadBit;
+			}
+			else if (buttons[e.dataIndex] == GLFW_RELEASE) {
+				gamepad.buttons &= ~e.gamepadBit;
+			}
+		}
+	}
+	else {
+		//有効なゲームパッドが接続されていないので、キーボード入力で代用
+
+		//配列インデックスとGamePadキーの対応表
+		static const struct {
+			int keyCode;
+			uint32_t gamepadBit;
+		} keyMap[] = {
+			{GLFW_KEY_J, GamePad::A},
+			{GLFW_KEY_K, GamePad::B},
+			{GLFW_KEY_U, GamePad::X},
+			{GLFW_KEY_I, GamePad::Y},
+			{GLFW_KEY_O, GamePad::L},
+			{GLFW_KEY_L, GamePad::R},
+			{GLFW_KEY_ENTER, GamePad::START},
+			{GLFW_KEY_W, GamePad::DPAD_UP},
+			{GLFW_KEY_A, GamePad::DPAD_DOWN},
+			{GLFW_KEY_S, GamePad::DPAD_LEFT},
+			{GLFW_KEY_D, GamePad::DPAD_RIGHT},
+		};
+
+		for (const auto& e : keyMap) {
+			const int key = glfwGetKey(window, e.keyCode);
+			if (key == GLFW_PRESS) {
+				gamepad.buttons |= e.gamepadBit;
+			}
+			else if (key == GLFW_RELEASE) {
+				gamepad.buttons &= ~e.gamepadBit;
+			}
+		}
+	}
+
+	//前回の更新で押されてなくて、今回押されているキーの情報をbuttonDownに格納
+	gamepad.buttonDown = gamepad.buttons & prevButtons;
+
+}
 
 
 } // namespace GLFWEW
--- OpenGL_3D_2019/Src/TitleScene.cpp
+++ OpenGL_3D_2019/Src/TitleScene.cpp
@@ -3,11 +3,16 @@
 */
 #include "TitleScene.h"
 #include "MainGameScene.h"
+#include "GLFWEW.h"
 
 /**
 * ƒvƒŒƒCƒ„[‚Ì“ü—Í‚ğˆ—‚·‚é
 */
 void TitleScene::ProcessInput()
 {
-	SceneStack::Instance().Replace(std::make_shared<MainGameScene>());
+	GLFWEW::Window& window = GLFWEW::Window::Instance();
+	if (window.GetGamePad().buttonDown & GamePad::START) {
+		SceneStack::Instance().Replace(std::make_shared<MainGameScene>());
+	}
+
 }
