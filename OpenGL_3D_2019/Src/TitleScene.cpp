99bbdb2c42e10f53cafb48509c812c2d1d15aac0
稜太 瀬川 <ryotan.113@outlook.jp>
2019-06-26 17:08:09 +0900

    TitleBg.tgaの追加 TitleScene,cppに変更及びコードの追加

--- OpenGL_3D_2019/Src/TitleScene.cpp
+++ OpenGL_3D_2019/Src/TitleScene.cpp
@@ -6,6 +6,23 @@
 #include "GLFWEW.h"
 
 /**
+* シーンを初期化する
+*
+* @retval true 初期化成功
+* @retval false 初期化失敗. ゲーム進行不可につき、プログラムを終了すること
+*/
+bool TitleScene::Initialize()
+{
+	spriteRenderer.Init(1000, "Res/Sprite.vert", "Res/Sprite.frag");
+	sprites.reserve(100);
+	Sprite spr(Texture::Image2D::Create("Res/TitleBg.tga"));
+	spr.Scale(glm::vec2(2));
+	sprites.push_back(spr);
+
+	return true;
+}
+
+/**
 * プレイヤーの入力を処理する
 */
 void TitleScene::ProcessInput()
@@ -15,4 +32,18 @@ void TitleScene::ProcessInput()
 		SceneStack::Instance().Replace(std::make_shared<MainGameScene>());
 	}
 
+}
+
+/**
+* シーンを更新する
+*
+* @param deltaTime 前回からの経過時間（秒）
+*/
+void TitleScene::Update(float deltaTime)
+{
+	spriteRenderer.BeginUpdate();
+	for (const Sprite& e : sprites) {
+		spriteRenderer.AddVertices(e);
+	}
+	spriteRenderer.EndUpdate();
 }
