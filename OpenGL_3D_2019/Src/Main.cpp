11f0102d75f0c8ec18bafe67670a31861ca3e92e
稜太 瀬川 <ryotan.113@outlook.jp>
2019-07-10 17:07:06 +0900

    main関数にバックバッファ消去するコード、エスケープキーを押したら終了するコードを追加

--- OpenGL_3D_2019/Src/Main.cpp
+++ OpenGL_3D_2019/Src/Main.cpp
@@ -17,6 +17,21 @@ int main()
 	for (;!window.ShouldClose();) {
 		const float deltaTime = 1.0f / 60.0f;
 		sceneStack.Update(deltaTime);
+
+		//ESCキーが押されたら終了ウィンドウを表示
+		if (window.IsKeyPressed(GLFW_KEY_ESCAPE)) {
+			if (MessageBox(nullptr, "ゲームを終了しますか？", "終了", MB_OKCANCEL) == IDOK) {
+				break;
+			}
+		}
+
+		//バックバッファを消去する
+		glClearColor(0.8f, 0.2f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//GLコンテキストのパラメーターを設定
+		glEnable(GL_CULL_FACE);
+		glEnable(GL_DEPTH_TEST);
 		sceneStack.Render();
 		window.SwapBuffers();
 	}
