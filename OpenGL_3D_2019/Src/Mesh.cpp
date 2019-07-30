a18dba4b55a88ccd10ae3ab55c4cbd28eef1bd1b
稜太 瀬川 <ryotan.113@outlook.jp>
2019-07-30 18:48:58 +0900

    メッシュを描画する関数を追加

--- OpenGL_3D_2019/Src/Mesh.cpp
+++ OpenGL_3D_2019/Src/Mesh.cpp
@@ -2,6 +2,8 @@
 * @file Mesh.cpp
 */
 #include "Mesh.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/constants.hpp>
 #include <iostream>
 
 /**
@@ -34,6 +36,8 @@ namespace Mesh
 		vboEnd = 0;
 		iboEnd = 0;
 		files.reserve(100);
+
+		AddCube("Cube");
 
 		return true;
 	}
@@ -177,6 +181,102 @@ namespace Mesh
 			return empty;
 		}
 		return itr->second;
+	}
+
+	/**
+	* 立方体を追加する
+	*
+	* @param name 立方体のメッシュ名
+	*/
+	void Buffer::AddCube(const char* name)
+	{
+		//			6---7			+Y -Z
+		//		   /|  /|			 |/
+		//		  / 5-/-4		-X --*-- +X
+		//		 3---2 /			/|
+		//		 |/  |/			 +Z -Y
+		//		 0---1
+		const glm::vec3 basePositions[] = {
+			{-1,-1,1},{1,-1,1},{1,1,1},{-1,1,1},
+			{1,-1,-1},{-1,-1,-1},{-1,1,-1},{1,1,-1},
+		};
+		const glm::vec2 baseTexCoords[] = { {0,1},{0,0},{1,0},{1,1} };
+		const glm::vec3 normals[] = {
+			{0,0,1},{1,0,0},{0,0,-1},{-1,0,0},
+			{0,-1,0},{0,1,0} };
+		const int planes[6][4] = {
+			{0,1,2,3},{1,4,7,2},{4,5,6,7},{5,0,3,6},
+			{5,4,1,0},{3,2,7,6} };
+		const GLubyte baseIndices[] = { 0,1,2,2,3,0 };
+
+		std::vector<Vertex> vertices;
+		vertices.reserve(4 * 6);
+		std::vector<GLubyte> indices;
+		indices.reserve(6 * 6);
+
+		//頂点データとインデックスデータを作成
+		for (size_t plane = 0; plane < 6; ++plane) {
+			for (size_t i = 0; i < 4; ++i) {
+				Vertex v;
+				v.position = basePositions[planes[plane][i]];
+				v.texCoord = baseTexCoords[i];
+				v.normal = normals[plane];
+				vertices.push_back(v);
+			}
+			for (size_t i = 0; i < 6; ++i) {
+				indices.push_back(static_cast<GLubyte>(baseIndices[i] + (plane * 4)));
+			}
+		}
+
+		//メッシュを追加
+		const size_t vOffset =
+			AddVertexData(vertices.data(), vertices.size() * sizeof(Vertex));
+		const size_t iOffset =
+			AddIndexData(indices.data(), indices.size() * sizeof(GLubyte));
+		const Primitive p =
+			CreatePrimitive(indices.size(), GL_UNSIGNED_BYTE, iOffset, vOffset);
+		const Material m = CreateMaterial(glm::vec4(1), nullptr);
+		AddMesh(name, p, m);
+	}
+
+	/**
+	* メッシュを追加する
+	*
+	* @param file 描画するファイル
+	* @param matVP 描画に使用するビュープロジェクション行列
+	* @param 描画に使用するモデル行列
+	*/
+	void Draw(const FilePtr& file, const glm::mat4 matVP, const glm::mat4& matM)
+	{
+		if (!file || file->meshes.empty() || file->materials.empty()) {
+			return;
+		}
+
+		const Mesh& mesh = file->meshes[0];
+		for (const Primitive& p : mesh.primitives) {
+			if (p.material < file->materials.size()) {
+				p.vao->Bind();
+				const Material& m = file->materials[p.material];
+				m.program->Use();
+				m.program->SetViewProjectionMatrix(matVP);
+				m.program->SetModelMatrix(matM);
+				glActiveTexture(GL_TEXTURE0);
+
+				//テクスチャがあるときは、そのテクスチャIDを設定する。ないときは０を設定する
+				if (m.texture) {
+					glBindTexture(GL_TEXTURE_2D, m.texture->Get());
+				}
+				else {
+					glBindTexture(GL_TEXTURE_2D, 0);
+				}
+
+				glDrawElementsBaseVertex(p.mode, p.count, p.type, p.indices, p.baseVertex);
+				p.vao->Unbind();
+			}
+		}
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		glUseProgram(0);
 	}
 
 }// namespace Mesh
--- OpenGL_3D_2019/Src/Mesh.h
+++ OpenGL_3D_2019/Src/Mesh.h
@@ -104,6 +104,8 @@ namespace Mesh {
 		Shader::ProgramPtr progStaticMesh;
 	};
 
+	void Draw(const FilePtr&, const glm::mat4& matVP, const glm::mat4& matM);
+
 
 } // namespace Mesh
 
