e170ee94496d5e5dd2850efde0ca6efc56b88108
稜太 瀬川 <ryotan.113@outlook.jp>
2019-07-31 18:11:24 +0900

    講義５回の復元成功

--- OpenGL_3D_2019/Src/Main.cpp
+++ OpenGL_3D_2019/Src/Main.cpp
@@ -8,22 +8,31 @@
 #include <iostream>
 int main()
 {
-	auto& window = GLFWEW::Window::Instance();
-	window.Init(800, 600, "Title");
+	GLFWEW::Window& window = GLFWEW::Window::Instance();
+	if (!window.Init(800, 600, "Title")) {
+		return 1;
+	}
 
 	SceneStack& sceneStack = SceneStack::Instance();
 	sceneStack.Push(std::make_shared<TitleScene>());
 
-	for (;!window.ShouldClose();) {
+	while (!window.ShouldClose())
+	{
+		//window.Update();
+
 		const float deltaTime = 1.0f / 60.0f;
 		sceneStack.Update(deltaTime);
 
+		/**
 		//ESCキーが押されたら終了ウィンドウを表示
 		if (window.IsKeyPressed(GLFW_KEY_ESCAPE)) {
 			if (MessageBox(nullptr, "ゲームを終了しますか？", "終了", MB_OKCANCEL) == IDOK) {
 				break;
 			}
 		}
+		*/
+
+
 
 		//バックバッファを消去する
 		glClearColor(0.8f, 0.2f, 0.1f, 1.0f);
@@ -32,7 +41,11 @@ int main()
 		//GLコンテキストのパラメーターを設定
 		glEnable(GL_CULL_FACE);
 		glEnable(GL_DEPTH_TEST);
+
+
 		sceneStack.Render();
 		window.SwapBuffers();
 	}
+
+	
 }
